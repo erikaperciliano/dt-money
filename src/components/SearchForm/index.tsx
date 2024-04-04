155fe7f3b5e74ae0bad588c92bac00480709ede7
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-04-04 18:12:33 +0100

    add visual structure of the project

---
+++ src/components/SearchForm/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SearchFormContainer } from './styles'
+import { MagnifyingGlass } from 'phosphor-react'
+
+export function SearchForm() {
+  return (
+    <SearchFormContainer>
+        <input type='text' placeholder='Busque por transações'/>
+        
+        <button type='submit'>
+            <MagnifyingGlass size={20}/>
+            Buscar
+        </button>
+    </SearchFormContainer>
+  )
+}
