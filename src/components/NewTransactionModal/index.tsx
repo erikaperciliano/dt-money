09d4f859df6b2cc882a40b3f67bf45fb9016a447
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-04-05 21:16:51 +0100

    add react hook form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,39 +2,83 @@ import React from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    //type: z.enum(['income', 'outcome'])
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-  return (
-    <Dialog.Portal>
-        <Overlay/>
-            <Content>
-            <Dialog.Title> New Transaction</Dialog.Title>
+    const { 
+        register, 
+        handleSubmit,
+        formState: { isSubmitting } 
+    } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema)
+    })
 
-            <CloseButton>
-                <X/>
-            </CloseButton>
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs){
+        await new Promise(resolve => setTimeout(resolve, 2000))
 
-            <form action="">
-                <input type="text" placeholder="Description" required />
-                <input type="number" placeholder="Price" required />
-                <input type="text" placeholder="Category" required />
+        console.log(data)
+    }
+
+    return (
+        <Dialog.Portal>
+            <Overlay/>
+                <Content>
+                <Dialog.Title> New Transaction</Dialog.Title>
+
+                <CloseButton>
+                    <X/>
+                </CloseButton>
+
+                <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                    <input 
+                        type="text"     
+                        placeholder="Description" 
+                        required
+                        {...register('description')} 
+                    />
+                    <input 
+                        type="number"   
+                        placeholder="Price" 
+                        required
+                        {...register('price', { valueAsNumber: true })} 
+                    />
+   
+                    <input 
+                        type="text"     
+                        placeholder="Category" 
+                        required
+                        {...register('category')} 
+                    />
+
                 
-                <TransactionType>
-                    <TransactionTypeButton variant='income' value='income'>
-                        <ArrowCircleUp size={24}/>
-                        Entrance
-                    </TransactionTypeButton>
+                    <TransactionType>
+                        <TransactionTypeButton variant='income' value='income'>
+                            <ArrowCircleUp size={24}/>
+                            Entrance
+                        </TransactionTypeButton>
 
-                    <TransactionTypeButton variant='outcome' value='outcome'>
-                        <ArrowCircleDown size={24}/>
-                        Exit
-                    </TransactionTypeButton>
-                </TransactionType>
+                        <TransactionTypeButton variant='outcome' value='outcome'>
+                            <ArrowCircleDown size={24}/>
+                            Exit
+                        </TransactionTypeButton>
+                    </TransactionType>
 
-                <button type='submit'>Register</button>
-            </form>
+                    <button type='submit' disabled={isSubmitting}>Register</button>
+                </form>
 
-        </Content>
-    </Dialog.Portal>
-  )
+            </Content>
+        </Dialog.Portal>
+    )
 }
